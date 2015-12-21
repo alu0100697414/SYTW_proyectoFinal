cff68653875afac80aae9df5a00a536dd22d6fd0
alu0100697414 <alu0100697414@ull.edu.es>
2015-12-21 22:05:46 +0000

    Añadido el uso de postgres para heroku

--- models/models.js
+++ models/models.js
@@ -1,12 +1,30 @@
 var path = require('path');
 
+// Postgres URL: postgres://user:pass@host:port/database
+// Sqlite URL:   sqlite://@/:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\/(.*)/);
+
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BBDD Sqlite
-var sequelize = new Sequelize(null, null, null, {
-  dialect: "sqlite",
-  storage: "quiz.sqlite"
+var sequelize = new Sequelize(DB_name, user, pwd, {
+  dialect: protocol,
+  protocol: protocol,
+  port: port,
+  host: host,
+  storage: storage, // solo sqlite
+  omitNull: true // solo postgres
 });
 
 // Importar la definicion de la tabla Quiz en quiz.js
