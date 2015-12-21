a9d2ab42955eaae02246ab80026215e1b0278922
alu0100697414 <alu0100697414@ull.edu.es>
2015-12-21 23:41:07 +0000

    Prueba para buscar error en Heroku con postgres

--- models/models.js
+++ models/models.js
@@ -3,7 +3,7 @@ var path = require('path');
 // Postgres URL: postgres://user:pass@host:port/database
 // Sqlite URL:   sqlite://@/:/
 
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\/(.*)/);
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 
 var DB_name = (url[6] || null);
 var user = (url[2] || null);
@@ -26,6 +26,8 @@ var sequelize = new Sequelize(DB_name, user, pwd, {
   storage: storage, // solo sqlite
   omitNull: true // solo postgres
 });
+
+console.log("eeeeeeeeeeeeeeoooooooooooooooooooo");
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
