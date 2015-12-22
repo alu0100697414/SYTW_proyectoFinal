1038e9b28b540a329dbdc53eaf05782f1450dd6b
alu0100697414 <alu0100697414@ull.edu.es>
2015-12-22 11:32:23 +0000

    Modificado el input del form y cambio en el metodo sync()

--- models/models.js
+++ models/models.js
@@ -33,7 +33,7 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // Exportar definicion de la tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
+sequelize.sync({ force: true }).success(function(){
   // success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().success(function(count){
     if(count === 0){ // La tabla se inicializa solo si esta vacia
